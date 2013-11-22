65c547939f2e2f023fcd0a63a34309d8dc8ccc76
bulatie <bulatie@gmail.com>
2013-11-22 15:15:56 +0800

    achieve enter website account Authenticate

---
+++ public/javascripts/js.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Bulatie on 13-11-19.
+ */
+$('.make-switch').on('switch-change', function () {
+
+});
---
+++ settings.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Bulatie on 13-11-22.
+ */
+module.exports = {
+    cookieSecret: 'jfgzgl',
+    db: 'jfgzgl',
+    host: 'localhost'
+}
