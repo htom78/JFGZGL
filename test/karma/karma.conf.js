916375c0b9dd5a6d0d43477b22978a3a34d84ac0
bulatie <bulatie@gmail.com>
2013-12-27 12:02:10 +0800

    use chrome to test

--- test/karma/karma.conf.js
+++ test/karma/karma.conf.js
@@ -72,7 +72,7 @@ module.exports = function(config) {
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel: config.LOG_DEBUG,
 
 
         // enable / disable watching file and executing tests whenever any file changes
@@ -87,7 +87,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
 
 
         // If browser does not capture in given timeout [ms], kill it
