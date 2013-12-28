51d4dbb9ecdc03b161851fb772da0105571a2508
bulatie <bulatie@gmail.com>
2013-12-28 15:55:02 +0800

    change to four buttons

--- public/js/controllers/machines.js
+++ public/js/controllers/machines.js
@@ -2,18 +2,18 @@ angular.module('mean.machines').controller('MachinesController', ['$scope', '$ro
         'Global', 'Machines', function ($scope, $routeParams, $location, $modal, Global, Machines) {
     $scope.global = Global;
     $scope.machines = {};
-    $scope.select = {
-        selectROn: true,
-        selectFOn: true,
-        selectROff: false,
-        selectFOff: false
-    };
+//    $scope.select = {
+//        selectROn: true,
+//        selectFOn: true,
+//        selectROff: false,
+//        selectFOff: false
+//    };
     $scope.useLog = {};
     $scope.machineId = {};
 
-/*
-* machines data init
-* */
+    /*
+    * machines data init
+    * */
     $scope.find = function() {
 
         Machines.query(function(pcStatuses) {
@@ -22,16 +22,21 @@ angular.module('mean.machines').controller('MachinesController', ['$scope', '$ro
 
     };
 
-/*
-* button enroll
-* */
+    /*
+    * button enroll
+    * */
     $scope.enroll = function (machineId) {
+
         $scope.machineId = machineId;
+
         var modalInstance = $modal.open({
             templateUrl: 'views/machines/enroll.html',
-            controller: function ($scope, Machines, $modalInstance, select, useLog, machineId) {
-                $scope.select = select;
+            controller: function ($scope, Machines, $modalInstance, useLog, machineId) {
+
+//                $scope.select = select;
+
                 $scope.machineId = machineId;
+
                 $scope.useLog = new Machines({
                     name: '',
                     sno: null,
@@ -41,25 +46,27 @@ angular.module('mean.machines').controller('MachinesController', ['$scope', '$ro
                 });
                 $scope.useLog.machineId = machineId;
 
+
                 $scope.ok = function () {
 
                     $scope.useLog.$save(function(response) {
                     });
 
                     $modalInstance.close(
-                        $scope.select.selectROn = false,
-                        $scope.select.selectROff = true
+//                        $scope.select.selectROn = false,
+//                        $scope.select.selectROff = true
                     );
                 };
 
                 $scope.cancel = function () {
                     $modalInstance.dismiss('cancel');
                 };
+
             },
             resolve: {
-                select:function () {
-                    return $scope.select;
-                },
+//                select:function () {
+//                    return $scope.select;
+//                },
                 useLog:function () {
                     return $scope.useLog;
                 },
@@ -70,61 +77,66 @@ angular.module('mean.machines').controller('MachinesController', ['$scope', '$ro
         });
 
         modalInstance.result.then(function (select) {
-            $scope.select = select;
+//            $scope.select = select;
         });
     };
 
-/*
- * button away
- * */
-$scope.away = function (machineId) {
-    $scope.machineId = machineId;
-    var modalInstance = $modal.open({
-        templateUrl: 'views/machines/away.html',
-        controller: function ($scope, Machines, $modalInstance, selectF, useLog, machineId) {
-            $scope.selectF = selectF;
-            $scope.machineId = machineId;
-            $scope.useLog = new Machines({
-                name: '',
-                sno: null,
-                tel: null,
-                machineId: null,
-                others: ''
-            });
-            $scope.useLog.machineId = machineId;
+    /*
+     * button away
+     * */
+    $scope.away = function (machineId) {
 
-            $scope.submit = function () {
+        $scope.machineId = machineId;
 
-                $scope.useLog.$save(function(response) {
-                });
-
-                $modalInstance.close($scope.selectF = true);
-            };
-        },
-        resolve: {
-            selectF:function () {
-                return $scope.selectF;
-            },
-            useLog:function () {
-                return $scope.useLog;
-            },
-            machineId:function () {
-                return $scope.machineId;
-            }
-        }
-    });
-
-    modalInstance.result.then(function (selected) {
-        $scope.selectF = selected;
-    });
-};
-
-/*
-* button fix
-* */
-    $scope.fix = function () {
         var modalInstance = $modal.open({
-            templateUrl: 'views/machines/fixR.html',
+            templateUrl: 'views/machines/away.html',
+            controller: function ($scope, Machines, $modalInstance, useLog, machineId) {
+
+                $scope.machineId = machineId;
+
+                $scope.useLog = new Machines({
+                    name: '',
+                    sno: null,
+                    tel: null,
+                    machineId: null,
+                    others: ''
+                });
+                $scope.useLog.machineId = machineId;
+
+
+                $scope.ok = function () {
+
+                    $scope.useLog.$save(function(response) {
+                    });
+
+                    $modalInstance.close($scope.selectF = true);
+                };
+
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+
+            },
+            resolve: {
+                useLog:function () {
+                    return $scope.useLog;
+                },
+                machineId:function () {
+                    return $scope.machineId;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selected) {
+        });
+    };
+
+    /*
+    * button fixOn
+    * */
+    $scope.fixOn = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'views/machines/fixOn.html',
             controller: function ($scope, $modalInstance) {
                 $scope.ok = function () {
                     $modalInstance.close();
@@ -138,9 +150,32 @@ $scope.away = function (machineId) {
             }
         });
 
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
+        modalInstance.result.then(function () {
+        });
+    };
+
+    /*
+     * button fixOff
+     * */
+    $scope.fixOff = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'views/machines/fixOff.html',
+            controller: function ($scope, $modalInstance) {
+                $scope.ok = function () {
+                    $modalInstance.close();
+                };
+
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            },
+            resolve: {
+            }
+        });
+
+        modalInstance.result.then(function () {
         });
     };
 
 }]);
+
