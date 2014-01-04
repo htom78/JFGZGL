8505950d2597a3417d3049e7856a19dd2886086c
bulatie <bulatie@gmail.com>
2014-01-05 02:21:31 +0800

    need async

--- public/js/controllers/situation.js
+++ public/js/controllers/situation.js
@@ -8,63 +8,18 @@ angular.module('mean.situation').controller('SituationController', ['$scope','$f
     /*
     * fetch situation data
     * */
-    $scope.gain = function() {
+     $scope.items = [];
+     $scope.gain = function() {
         Situation.query(function(situations) {
-            $scope.situations = situations;
-//            $scope.items = situations;
+            $scope.items = situations;
         });
     };
 
-
-        $scope.items = [
-            {"id":"1","name":"name 1","description":"description 1","field3":"field3 1","field4":"field4 1","field5 ":"field5 1"},
-            {"id":"2","name":"name 2","description":"description 1","field3":"field3 2","field4":"field4 2","field5 ":"field5 2"},
-            {"id":"32","name":"name 3","description":"description 1","field3":"field3 3","field4":"field4 3","field5 ":"field5 3"},
-            {"id":"41","name":"name 4","description":"description 1","field3":"field3 4","field4":"field4 4","field5 ":"field5 4"},
-            {"id":"52","name":"name 5","description":"description 1","field3":"field3 5","field4":"field4 5","field5 ":"field5 5"},
-            {"id":"61","name":"name 6","description":"description 1","field3":"field3 6","field4":"field4 6","field5 ":"field5 6"},
-            {"id":"7","name":"name 7","description":"description 1","field3":"field3 7","field4":"field4 7","field5 ":"field5 7"},
-            {"id":"8","name":"name 8","description":"description 1","field3":"field3 8","field4":"field4 8","field5 ":"field5 8"},
-            {"id":"9","name":"name 9","description":"description 1","field3":"field3 9","field4":"field4 9","field5 ":"field5 9"},
-            {"id":"10","name":"name 10","description":"description 1","field3":"field3 10","field4":"field4 10","field5 ":"field5 10"},
-            {"id":"11","name":"name 11","description":"description 1","field3":"field3 11","field4":"field4 11","field5 ":"field5 11"},
-            {"id":"12","name":"name 12","description":"description 1","field3":"field3 12","field4":"field4 12","field5 ":"field5 12"},
-            {"id":"13","name":"name 13","description":"description 1","field3":"field3 13","field4":"field4 13","field5 ":"field5 13"},
-            {"id":"14","name":"name 14","description":"description 1","field3":"field3 14","field4":"field4 14","field5 ":"field5 14"},
-            {"id":"15","name":"name 15","description":"description 1","field3":"field3 15","field4":"field4 15","field5 ":"field5 15"},
-            {"id":"16","name":"name 16","description":"description 1","field3":"field3 16","field4":"field4 16","field5 ":"field5 16"},
-            {"id":"17","name":"name 17","description":"description 1","field3":"field3 17","field4":"field4 17","field5 ":"field5 17"},
-            {"id":"18","name":"name 18","description":"description 1","field3":"field3 18","field4":"field4 18","field5 ":"field5 18"},
-            {"id":"19","name":"name 19","description":"description 1","field3":"field3 19","field4":"field4 19","field5 ":"field5 19"},
-            {"id":"20","name":"name 20","description":"description 1","field3":"field3 20","field4":"field4 20","field5 ":"field5 20"}
-        ];
-
-        $scope.sortables = [
-            {
-                label: 'Unsorted',
-                val: 'none'
-            },
-            {
-                label: 'Name',
-                val: 'name'
-            },
-            {
-                label: 'id',
-                val: 'id'
-            },
-            {
-                label: 'note',
-                val: 'note'
-            }
-        ];
-        $scope.sortPrep = 'none';
-
-
+    $scope.gain();
 
     /*
     * PaginationController
     * */
-
 
         $scope.filteredItems = [];
         $scope.groupedItems = [];
